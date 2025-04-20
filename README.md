d6e3eb6359bbf06db27c86e0159d0be6e224cdfa
Benjamin Larkey <spartinofarrows@gmail.com>
2025-04-20 11:43:34 -0500

    vulkan init

---
+++ README.md
@@ -0,0 +1,22 @@
+## Playing with Vulkan API C++
+
+### Install Options
+
+<p> 1: Linux / Mac? </p>
+
+```bash
+sudo apt update
+sudo apt install libvulkan-dev vulkan-headers vulkan-tools glslang-tools
+sudo apt install build-essential cmake libglm-dev libglfw3-dev
+mkdir build
+cd build
+cmake ..
+make
+./VulkanTest
+```
+
+<p> 2: Windows </p>
+
+```bash
+...
+```
---
+++ main.cpp
@@ -0,0 +1,90 @@
+// C++ Gui Program
+
+// VULKAN HEADER   
+#define GLFW_INCLUDE_VULKAN 
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+#include <stdexcept>
+#include <vector>
+#include <cstring>
+
+class VulkanApplication {
+public:
+    void run() {
+        initWindow();
+        initVulkan();
+        mainLoop();
+        cleanup();
+    }
+
+private:
+    GLFWwindow* window; // pointer to the window
+    VkInstance instance;
+    
+    void initWindow() {
+        glfwInit();
+        //glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API); // doesn't seem required
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+        window = glfwCreateWindow(800, 600, "Test", nullptr, nullptr);
+    }
+    
+    void initVulkan() {
+        createInstance();
+    }
+    
+    void createInstance() {
+        VkApplicationInfo appInfo{};
+
+        // would like to know more about this info and where it's held
+        appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        appInfo.pApplicationName = "Hello Vulkan";
+        appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.pEngineName = "No Engine";
+        appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.apiVersion = VK_API_VERSION_1_0;
+        
+        // more config....
+        VkInstanceCreateInfo createInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        createInfo.pApplicationInfo = &appInfo;
+        
+        uint32_t glfwExtensionCount = 0;
+        const char** glfwExtensions;
+        glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+        
+        createInfo.enabledExtensionCount = glfwExtensionCount;
+        createInfo.ppEnabledExtensionNames = glfwExtensions;
+        createInfo.enabledLayerCount = 0;
+        
+        // error handling for the instance
+        if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
+            throw std::runtime_error("failed to create instance!");
+        }
+    }
+    
+    void mainLoop() {
+        while (!glfwWindowShouldClose(window)) {
+            glfwPollEvents();
+        }
+    }
+    
+    void cleanup() {
+        vkDestroyInstance(instance, nullptr);
+        glfwDestroyWindow(window);
+        glfwTerminate();
+    }
+};
+
+int main() {
+    VulkanApplication app;
+    
+    try {
+        app.run();
+    } catch (const std::exception& e) {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;                            // return 1
+    }
+    
+    return EXIT_SUCCESS;                                // return 0
+}
