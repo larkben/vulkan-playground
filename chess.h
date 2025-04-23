867b796c6bf7ebb964cda9539fd4f5ff070d3e14
Benjamin <60051088+larkben@users.noreply.github.com>
2025-04-23 20:20:47 +0000

    build: adding chess and chess pieces

--- chess.h
+++ chess.h
@@ -1,11 +1,33 @@
 // Chess Class Header File
 
+#include <vector>
+#include <pair> // should look into this
+
+enum GamePieces {
+    king = 0, 
+    queen = 1,
+    rook = 2,
+    knight = 3,
+    bishop = 4,
+    pawn = 5
+}
+
+// chess board
 class Chess {
     public:
 
         Chess(); // constructor
+        void drawBoard(); // draw the board
 
     private:
 
-        void drawBoard(); // draw the board
+        vector<pair<int, >> whitePieces; // locations
+        vector<pair<int, >> blackPieces; // locations
+
+        void drawAllPossiblePositions(ChessPiece piece); 
+};
+
+// chess piece ~ rational?
+class ChessPiece {
+    
 }
